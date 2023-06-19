8b8503650e2a62711da861bae138892548bf1da5
nanak <sidhu.nds22@gmail.com>
2023-06-19 20:13:56 +0530

    server rendering layout and rendering statergies

--- asps/app/components/Navbar.js
+++ asps/app/components/Navbar.js
@@ -1,26 +1,23 @@
 "use client"
-import { setLazyProp } from "next/dist/server/api-utils"
-import Image from "next/image"
-import { useEffect,useRef, useState } from "react"
-import { useTheme } from "next-themes"
 import ThemeToggle from "./ThemeToggle"
 import Logo from "./Logo"
+import { useState } from "react"
+import Link from "next/link"
 
 export default function Navbar() {
-
   return (
     <div className=" mx-8 mt-11 flex items-center justify-between">
         <div className="w-40 h-28">
             <Logo></Logo>
         </div>
         <div className="w-1/2 flex items-center">
-            <a className="h-full px-6 text-lg">Home</a>
-            <a className="h-full px-6 text-lg">About</a>
-            <a className="h-full px-6 text-lg">Blogs</a>
+            <Link className="h-full px-6 text-lg" href="/">Home</Link>
+            <Link className="h-full px-6 text-lg" href="#">About</Link>
+            <Link className="h-full px-6 text-lg" href="#" >Blogs</Link>
         </div>
         <div className="grow flex justify-end items-center">
-        <a className="h-full px-6 text-lg  text-purple-500 hover:text-purple-800 dark:text-emerald-300 dark:hover:text-emerald-500">Sign in</a>
-        <a className="h-full px-6 text-lg text-purple-500 hover:text-purple-800 dark:text-emerald-300 dark:hover:text-emerald-500">Register</a>
+        <Link className="h-full px-6 text-lg  text-purple-500 hover:text-purple-800 dark:text-emerald-300 dark:hover:text-emerald-500" href="/login">Sign in</Link>
+        <Link className="h-full px-6 text-lg text-purple-500 hover:text-purple-800 dark:text-emerald-300 dark:hover:text-emerald-500" href="#">Register</Link>
         <div className=" px-6">
         <ThemeToggle></ThemeToggle>
         </div>
--- asps/app/layout.js
+++ asps/app/layout.js
@@ -1,21 +1,18 @@
-"use client"
-
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Navbar from './components/Navbar'
-import { ThemeProvider } from "next-themes"
+import NextThemeProvider from './NextThemeProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({ children }) {
-
   return (
-    <html lang="en">
+    <html lang="en" >
       <body className={inter.className}>
-      <ThemeProvider attribute="class">
-        <Navbar></Navbar>
-        {children}
-      </ThemeProvider>
+        <NextThemeProvider>
+          <Navbar></Navbar>
+          {children}
+        </NextThemeProvider>
         </body>
     </html>
   )
